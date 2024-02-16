1c04a07e2f8b4501a44460005a506cbf748a0660
Swaroop-6291 <swaroop6291@gmail.com>
2024-02-16 12:42:50 +0530

    add register

--- src/app.js
+++ src/app.js
@@ -14,5 +14,8 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+import router from "./routes/user.routes.js";
+
+app.use("api/v1/user",router)
 
 export {app}
